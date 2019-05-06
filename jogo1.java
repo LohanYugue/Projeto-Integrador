6d0e3ffffe5ea6f2e6533a312edbf61b836ce368
LohanYugue <42221461+LohanYugue@users.noreply.github.com>
2019-05-06 10:17:11 -0300

    Atualização jogo1.java
    Incluído alguns caminhos no programa
    Alterado a função salaEAD para salaCC  e incluído as 5 questões da matéria

--- jogo1.java
+++ jogo1.java
@@ -38,7 +38,7 @@ static Scanner leitor = new Scanner(System.in);
             inicio = leitor.nextLine().toLowerCase().charAt(0);
         }
         switch (inicio) {
-            case 'a': salaEAD();
+            case 'a': start();
                 break;
             case 'b':
                 break;
@@ -52,13 +52,75 @@ static Scanner leitor = new Scanner(System.in);
         }
     }
     
-    public static void salaEAD() {
+    public static void start() {
+        System.out.print(" Bem vindo ao jogo RPG “Escola do Caos”,\n antes de mais nada, "
+                + "nos informe qual será seu nome neste jogo: ");
+        String nome = leitor.nextLine().toUpperCase();
+        System.out.print("\n" +
+        "Elfo\n" +
+        "Mago\n" +
+        "Besta\n" +
+        "Humano\n"+
+        "\nMuito bem "+nome+", agora digite o personagem você deseja ser:");
+        String personagem = leitor.nextLine().toUpperCase();
+        while(!personagem.equals("ELFO")&&!personagem.equals("MAGO")&&!personagem.equals("BESTA")&&!personagem.equals("HUMANO")){
+            System.out.print("Personagem inválido, digite um dos personagens acima: ");
+            personagem = leitor.nextLine().toUpperCase();
+        }
+        System.out.println("\n");
+        System.out.println("\n");
+        System.out.println(nome+", você é um "+personagem+ ", que vive em uma terra misteriosa chamada "
+        + "Monsaraz, um lugar belo e pacífico,\n contudo, descobriu que sua mãe Freya adquiriu uma doença "
+        + "grave e muito rara que não tem cura,\n porém, através de muitas pesquisas e perguntas "
+        + "feitas aos habitantes da região,\n descobriu que há uma pedra mágica com poderes curadores, "
+        + "localizado além das terras de Monsaraz,\n em uma antiga escola, isolada nas montanhas.");
+        System.out.println("\n");
+        System.out.println("\n");
+        
+        System.out.print("Você parte em busca da pedra, após 4 longas horas de caminhada você chega a seu destino e encontra a escola que fica no fim de uma longa ponte.\n"
+                + "Após passar pela ponte, você se depera com uma grande porta já aberta! Adentra naquele ambiente inóspito e de repente a porta se fecha.\n"
+                + "Há algo gravado na porta dizendo .....\n"
+                + "*****************************\n"
+                + "*****************************\n"
+                + "Para sair, encontre os 5 emblemas.\n"
+                + "*****************************\n"
+                + "*****************************\n"
+                + "Olha com mais atenção na porta e percebe que há 5 espaços com formato octogonal vazios, como se faltasse algo!\n"
+                + "Você precisa encontrar a pedra mágica e os cinco emblemas para salvar sua mãe!\n"
+                + "Há dois corredores para seguir\n"
+                + "Esquerda\n"
+                + "Direita\n"
+                + "Qual você deseja seguir? ");
+                String escolha = leitor.nextLine().toLowerCase();
+                while(!escolha.equals("esquerda")&&!escolha.equals("direita")){
+                    System.out.print("Você possui somente duas opções, ir para direita ou para esquerda.\n Digite a direção que deseja seguir: ");
+                    escolha = leitor.nextLine().toLowerCase();
+                }
+                if (escolha.equals("esquerda")){
+                    System.out.print("Você segue em direção ao corredor da esquerda e há duas salas com a descrição.\n"
+                            + "ConceitosDeComputacao\n"
+                            + "Algoritmos\n"
+                            + "Qual sala você deseja entrar? ");
+                    String escolha2 = leitor.nextLine().toLowerCase();
+                    while(!escolha2.equals("conceitosdecomputacao")&&!escolha2.equals("algoritmos")){
+                    System.out.print("Você possui somente duas opções, ir para ConceitosDeComputacao ou para Algoritmos.\n Digite qual sala você deseja entrar: ");
+                    escolha2 = leitor.nextLine().toLowerCase();
+                    }
+                    
+                    if (escolha2.equals("conceitosdecomputacao")){
+                        salaCC();
+                    }
+                }
+        
+    }
+    
+    public static void salaCC() {
         
         //Questão 1   
         do{
             System.out.println ("*******Questão 1*******");
-            System.out.println ("A soma de 5 + 5 resulta em um número ....?");
-            System.out.println("(a)Par\n(b)Impar\n(c)Horizontal\n(d)Vertical");
+            System.out.println ("O resultado da multiplicação dos números decimais 3 e 4  pode ser representado em hexadecimal pela letra:");
+            System.out.println("(a)B\n(b)F\n(c)C\n(d)A");
             System.out.print("Escolha a alternativa correta: ");
             opcao = leitor.nextLine().toLowerCase().charAt(0);
         
@@ -66,20 +128,20 @@ static Scanner leitor = new Scanner(System.in);
                 System.out.println("Resposta inválida, favor informar novamente a resposta");
                 opcao = leitor.nextLine().toLowerCase().charAt(0); 
             }
-            if (opcao=='a'){
+            if (opcao=='c'){
                 acertou();
             }
-            else if ((opcao=='b') || (opcao=='c') || (opcao=='d')){
+            else if ((opcao=='b') || (opcao=='a') || (opcao=='d')){
                 errou();
             }
-        }while(opcao!='a'&&soma!=0);
+        }while(opcao!='c'&&soma!=0);
     
     
         //Questão 2
         do{
             System.out.println ("*******Questão 2*******");
-            System.out.println ("O palmeiras tem Mundial?");
-            System.out.println("(a)Sim\n(b)Não\n(c)Com certeza\n(d)Tem só se for roubado");
+            System.out.println ("A função “NÃO E” também pode ser escrita da seguinte forma:");
+            System.out.println("(a)AND\n(b)NOR\n(c)OR\n(d)NAND");
             System.out.print("Escolha a alternativa correta: ");
             opcao = leitor.nextLine().toLowerCase().charAt(0);
         
@@ -99,8 +161,8 @@ static Scanner leitor = new Scanner(System.in);
         //Questão 3
         do{
             System.out.println ("*******Questão 3*******");
-            System.out.println ("Em que ano se deu início a ditadura no Brasil?");
-            System.out.println("(a)1945\n(b)1956\n(c)1964\n(d)No século XXI");
+            System.out.println ("Qual símbolo representa a função AND/E ?");
+            System.out.println("(a)+\n(b)-\n(c)*\n(d)/");
             System.out.print("Escolha a alternativa correta: ");
             opcao = leitor.nextLine().toLowerCase().charAt(0);
         
@@ -120,8 +182,8 @@ static Scanner leitor = new Scanner(System.in);
         //Questão 4
         do{
             System.out.println ("*******Questão 4*******");
-            System.out.println ("Quanto vale em hexadecimal o número binário 1011?");
-            System.out.println("(a)F\n(b)B\n(c)D\n(d)C");
+            System.out.println ("Quanto vale em decimal o número binário 100000100?");
+            System.out.println("(a)254\n(b)260\n(c)256\n(d)512");
             System.out.print("Escolha a alternativa correta: ");
             opcao = leitor.nextLine().toLowerCase().charAt(0);
         
@@ -141,7 +203,7 @@ static Scanner leitor = new Scanner(System.in);
         //Questão 5
         do{
             System.out.println ("*******Questão 5*******");
-            System.out.println ("Qual símbolo representa o operador lógico OR/OU ?");
+            System.out.println ("Qual símbolo representa a função OR/OU ?");
             System.out.println("(a)+\n(b)*\n(c)-\n(d)/");
             System.out.print("Escolha a alternativa correta: ");
             opcao = leitor.nextLine().toLowerCase().charAt(0);
